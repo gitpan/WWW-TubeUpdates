d44f9ad8e380c981fff3e6a98fe6e8a3af68c8dd
Mohammad S Anwar <mohammad.anwar@yahoo.com>
2011-08-08 12:26:17 +0000

    Import of MANWAR/WWW-TubeUpdates-0.02 from CPAN.
    gitpan-cpan-distribution: WWW-TubeUpdates
    gitpan-cpan-version:      0.02
    gitpan-cpan-path:         MANWAR/WWW-TubeUpdates-0.02.tar.gz
    gitpan-cpan-author:       MANWAR
    gitpan-cpan-maturity:     released

--- lib/WWW/TubeUpdates.pm
+++ lib/WWW/TubeUpdates.pm
@@ -18,11 +18,11 @@ WWW::TubeUpdates - Interface to Tube Updates API.
 
 =head1 VERSION
 
-Version 0.01
+Version 0.02
 
 =cut
 
-our $VERSION = '0.01';
+our $VERSION = '0.02';
 Readonly my $BASE_URL   => 'http://api.tubeupdates.com/?method=get.status';
 Readonly my $TUBE_LINES =>
 {
